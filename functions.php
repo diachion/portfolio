41b989e2f06a42e5136aabc1bd15fb5b89a308c9
diachion <lydiaquon@gmail.com>
2021-01-17 16:07:40 -0800

    added isotope.js

--- functions.php
+++ functions.php
@@ -12,7 +12,8 @@ function salient_child_enqueue_styles() {
 		wp_enqueue_style(  'lightbox2',  get_stylesheet_directory_uri(). '/includes/lightbox2-dev/dist/css/lightbox.min.css', '', $nectar_theme_version );
 		wp_enqueue_script( 'lightbox2', get_stylesheet_directory_uri() . '/includes/lightbox2-dev/dist/js/lightbox.min.js', '', $nectar_theme_version );
 
-		wp_enqueue_script( 'global', get_stylesheet_directory_uri() . '/js/global.js', '', $nectar_theme_version );
+		wp_enqueue_script( 'isotope', get_stylesheet_directory_uri() . '/js/isotope.min.js', '', $nectar_theme_version );
+		wp_enqueue_script( 'global', get_stylesheet_directory_uri() . '/js/global.js', '', $nectar_theme_version,1 );
 		
     if ( is_rtl() ) {
 			 wp_enqueue_style(  'salient-rtl',  get_template_directory_uri(). '/rtl.css', array(), '1', 'screen' );
--- js/global.js
+++ js/global.js
@@ -3,8 +3,17 @@ new WOW().init();
 
 /* Functions */
 (function($) {
-  
 
+  $('.blog-recent').isotope({
+    itemSelector: '.type-post',
+  });
+
+  $('.project-filter').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('filter');
+    // use filterFn if matches value
+    // filterValue = filterFns[ filterValue ] || filterValue;
+    $('.blog-recent').isotope({ filter: filterValue });
+  });
 
 
 
