92e11a759cf1a5f34133b4dcb625d9308ff6b954
diachion <lydiaquon@gmail.com>
2021-01-22 19:59:32 -0800

    trying to fix isotope.js

--- js/global.js
+++ js/global.js
@@ -4,31 +4,24 @@ new WOW().init();
 /* Functions */
 (function($) {
   (function($) { // Tell wordpress that everything in here is going to use Jquery
-    $(document).ready(function(){
-        // Initiate Isotope
+    // Initiate Isotope
+    $('.blog-recent').imagesLoaded( function() {
         $('.blog-recent').isotope({
             itemSelector: '.col',
             layoutMode: 'fitRows'
         });
-        // Bind the click event
-        $('.project-filter button').on( 'click',function(){
-            var filterValue = $( this ).attr('filter');
-            $('.blog-recent').isotope({ filter: filterValue });
-
-            $(this).addClass('selected');
-            $(this).siblings().removeClass('selected');
-
-            console.log('hi');
-        });
-        // Run as soon as all the images finish loading
-        $(window).load(function(){
-            $('.blog-recent').isotope({filter: ''});
-        });
-    }); // Document.ready
-});
-});
-
-(jQuery);
+        $('.blog-recent').isotope({filter: ''});
+    });
+    // Bind the click event
+    $('.project-filter').on('click','button',function(){
+        var filterValue = $(this).attr('filter');
+        $('.blog-recent').isotope({ filter: filterValue });
+        $(this).addClass('selected');
+        $(this).siblings().removeClass('selected');
+        console.log('WORK DAMNIT');
+    });
+  });
+});(jQuery);
 
 // Lightbox
 
