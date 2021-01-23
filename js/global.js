ae441b5e88a2c779deebb336381aa635e52224b4
diachion <lydiaquon@gmail.com>
2021-01-23 15:34:56 -0800

    fixed the js file

--- js/global.js
+++ js/global.js
@@ -3,25 +3,26 @@ new WOW().init();
 
 /* Functions */
 (function($) {
-  (function($) { // Tell wordpress that everything in here is going to use Jquery
+
     // Initiate Isotope
-    $('.blog-recent').imagesLoaded( function() {
-        $('.blog-recent').isotope({
-            itemSelector: '.col',
-            layoutMode: 'fitRows'
-        });
-        $('.blog-recent').isotope({filter: ''});
+    $('.blog-recent').imagesLoaded(function() {
+      $('.blog-recent').isotope({
+        itemSelector: '.col',
+        layoutMode: 'fitRows'
+      });
+      $('.blog-recent').isotope({filter: ''});
     });
+
     // Bind the click event
     $('.project-filter').on('click','button',function(){
-        var filterValue = $(this).attr('filter');
-        $('.blog-recent').isotope({ filter: filterValue });
-        $(this).addClass('selected');
-        $(this).siblings().removeClass('selected');
-        console.log('WORK DAMNIT');
+      var filterValue = $(this).attr('filter');
+      $('.blog-recent').isotope({ filter: filterValue });
+      $(this).addClass('selected');
+      $(this).siblings().removeClass('selected');
     });
-  });
-});(jQuery);
+
+
+})(jQuery);
 
 // Lightbox
 
